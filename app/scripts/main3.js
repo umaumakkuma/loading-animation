3970918fc6db330dc8c75d713333cd5c90834785
Yusuke.Nomura <umaumakkuma@gmail.com>
2020-09-19 01:34:10 +0900

    WIP

--- app/scripts/main3.js
+++ app/scripts/main3.js
@@ -3,21 +3,24 @@
 
   window.onload = () => {
     const loading = document.getElementById('js-loading')
+    const loadingTop = document.getElementById('js-loading-top')
+    const loadingBottom = document.getElementById('js-loading-bottom')
     const loadingIcon = document.getElementById('js-loading-icon')
     const loadingCompleteText = document.getElementById('js-loading-complete-text')
 
     // クルクルローディングアイコンを非表示にする
-    loadingIcon.classList.add('fadeout')
+    loadingIcon.classList.add('fade-out')
 
     // クルクルアイコンの非表示アニメーションが終了したら
     loadingIcon.addEventListener('animationend', () => {
-      loadingIcon.classList.add('d-none')
-      loadingCompleteText.classList.add('fadein')
+      loadingCompleteText.classList.add('fade-in-out')
     })
 
     // completeテキストアニメーションが終了したら
     loadingCompleteText.addEventListener('animationend', () => {
-      loading.classList.add('open')
+      loadingTop.classList.add('open')
+      loadingBottom.classList.add('open')
+      loading.classList.add('d-none')
     })
   };
 }());
