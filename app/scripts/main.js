6b47cd9c6f0369bdc75917152c500d184b6d7722
Yusuke.Nomura <umaumakkuma@gmail.com>
2020-09-24 01:05:48 +0900

    fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,23 @@
-(function () {
+(function() {
   'use strict'
 
-  window.onload = () => {
-    const loading = document.getElementById('js-loading')
-    const loadingIcon = document.getElementById('js-loading-icon')
-    const loadingCompleteText = document.getElementById('js-loading-complete-text')
+  window.onload = function() {
+    const loading = document.getElementById('js-loading');
+    const loadingIcon = document.getElementById('js-loading-icon');
+    const loadingCompleteText = document.getElementById('js-loading-complete-text');
 
-    // クルクルローディングアイコンを非表示にする
-    loadingIcon.classList.add('fade-out')
+    // ローディングアイコンをフェードアウトさせる
+    loadingIcon.classList.add('fade-out');
 
     // クルクルアイコンの非表示アニメーションが終了したら
-    loadingIcon.addEventListener('animationend', () => {
-      loadingIcon.classList.add('d-none')
-      loadingCompleteText.classList.add('fade-in')
+    loadingIcon.addEventListener('animationend', function() {
+      loadingIcon.classList.add('d-none');
+      loadingCompleteText.classList.add('fade-in');
     })
 
     // completeテキストアニメーションが終了したら
-    loadingCompleteText.addEventListener('animationend', () => {
-      loading.classList.add('open')
+    loadingCompleteText.addEventListener('animationend', function() {
+      loading.classList.add('open');
     })
   };
 }());
--- app/scripts/main3.js
+++ app/scripts/main3.js
@@ -1,31 +1,30 @@
-(function () {
+(function() {
   'use strict'
 
-  window.onload = () => {
-    const loading = document.getElementById('js-loading')
-    const loadingTop = document.getElementById('js-loading-top')
-    const loadingBottom = document.getElementById('js-loading-bottom')
-    const loadingIcon = document.getElementById('js-loading-icon')
-    const loadingCompleteText = document.getElementById('js-loading-complete-text')
+  window.onload = function()  {
+    const loading = document.getElementById('js-loading');
+    const loadingTop = document.getElementById('js-loading-top');
+    const loadingBottom = document.getElementById('js-loading-bottom');
+    const loadingIcon = document.getElementById('js-loading-icon');
+    const loadingCompleteText = document.getElementById('js-loading-complete-text');
 
     // クルクルローディングアイコンを非表示にする
-    loadingIcon.classList.add('fade-out')
+    loadingIcon.classList.add('fade-out');
 
     // クルクルアイコンの非表示アニメーションが終了したら
-    loadingIcon.addEventListener('animationend', () => {
-      loadingCompleteText.classList.add('fade-in-out')
+    loadingIcon.addEventListener('animationend', function() {
+      loadingCompleteText.classList.add('fade-in-out');
     })
 
     // completeテキストアニメーションが終了したら
-    loadingCompleteText.addEventListener('animationend', () => {
-      loadingTop.classList.add('open')
-      loadingBottom.classList.add('open')
+    loadingCompleteText.addEventListener('animationend', function() {
+      loadingTop.classList.add('open');
+      loadingBottom.classList.add('open');
     })
 
     // コンテンツオープンアニメーションが終了したら
-    loadingTop.addEventListener('animationend', () => {
-      loading.classList.add('d-none')
+    loadingTop.addEventListener('animationend', function() {
+      loading.classList.add('d-none');
     })
-
   };
 }());
